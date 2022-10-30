514de4c763904b0e3a7439344c1425f7d3e79e12
Gianni Ridwan <99036552@mydavinci.nl>
2022-10-30 23:33:27 +0100

    header footer

---
+++ components/header.php
@@ -0,0 +1,12 @@
+<head>
+  <title>ToDo List</title>
+  <link rel="stylesheet" href="../style.css">
+  <script src="https://kit.fontawesome.com/198c4d6390.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand"><i class="fa-solid fa-note-sticky" style="padding-right: 4px;"></i>Notities</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <!DOCTYPE html>
-<html
+<html lang="en">
+    <head>
+        <title>ToDo List</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+        <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand"><i class="fa-solid fa-note-sticky" style="padding-right: 4px;"></i>Notities</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                    <a class="nav-link" href="script/makeList.php">Lijst maken<span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+            </nav>
+        </header>
+    </body>
+    <footer>
+        <?php   
+            include("components/footer.php"); 
+        ?>
+    </footer>
+</html>
